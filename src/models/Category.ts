ba513acdca4bd1a05d73815f697363310409f0c0
stfaaydo <fatih.b.aydogan@stud.h-da.de>
2023-10-31 19:59:18 +0100

    sadfa

--- src/models/Category.ts
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from 'mongoose';
-
-const categorySchema = new Schema({
-  name: String,
-  description: String,
-  recipes: [{ type: Schema.Types.ObjectId, ref: 'Recipe' }],
-});
-
-export default model('Category', categorySchema);
